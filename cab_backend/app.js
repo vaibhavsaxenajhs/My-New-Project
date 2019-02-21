eb96d1718d5b719648d0844d4f3001577bb832af
vaibhavsaxenajhs <47653356+vaibhavsaxenajhs@users.noreply.github.com>
2019-02-21 16:42:17 +0530

    Update app.js

--- cab_backend/app.js
+++ cab_backend/app.js
@@ -14,16 +14,9 @@ const adminRouter= require('./Router/adminRouter');
 const driverRouter = require('./Router/driverRouter')
 const config = require('./Config/config.js')
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-
 app.use('/user',userRouter);
 app.use('/admin',adminRouter);
 app.use('/driver',driverRouter);
-
-
 
 app.listen(config.port, function (err, result) { 
     if (err) { 
